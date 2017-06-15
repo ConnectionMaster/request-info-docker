0cad5c8575416dfd0807f187a0e9f6098de93ef8
Sergei Goshko <sergei.goshko@elavon.com>
2017-06-15 11:22:22 -0400

    add details

--- main.go
+++ main.go
@@ -7,17 +7,40 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "<html><body>");
 
+    fmt.Fprintln(w, "<i>Request Host</i>: ", r.Host, "</br>")
+    fmt.Fprintln(w, "<i>Request URL</i>: ", r.URL, "</br>")
+    fmt.Fprintln(w, "<i>Context path</i>: ", r.URL.Path[1:], "</br>")
+    fmt.Fprintln(w, "<i>Client IP (RemoteAddr)</i>: ", r.RemoteAddr, "</br>")
+    fmt.Fprintln(w, "<i>Request TLS</i>: ", r.TLS, "</br>")
+
+    fmt.Fprintln(w, "</br><b>Request Headers:</b></br>")
     var keys []string
     for k := range r.Header {
         keys = append(keys, k)
     }
     sort.Strings(keys)
-    
-    fmt.Fprintln(w, "<b>Request Headers:</b></br>", r.URL.Path[1:])
+    fmt.Fprintln(w, "<ul>")
     for _, k := range keys {
-        fmt.Fprintln(w, k, ":", r.Header[k], "</br>", r.URL.Path[1:])
+        fmt.Fprintln(w, "<li><i>", k, "</i>:", r.Header[k], "</li>")
     }
+    fmt.Fprintln(w, "</ul>")
+
+    fmt.Fprintln(w, "</br><b>Form parameters:</b></br>")
+    r.ParseForm()
+    keys = []string{}
+    for k := range r.Form {
+        keys = append(keys, k)
+    }
+    sort.Strings(keys)
+    fmt.Fprintln(w, "<ul>")
+    for _, k := range keys {
+        fmt.Fprintln(w, "<li><i>", k, "</i>:", r.Form[k], "</li>")
+    }
+    fmt.Fprintln(w, "</ul>")
+
+    fmt.Fprintln(w, "</body></html>");
 }
 
 func main() {
